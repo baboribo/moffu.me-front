d2e70a502191341f539edc675bdeab8855eaa080
Uroyo <inertia_mailer_0c@icloud.com>
2025-03-23 19:51:23 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="m-5 p20 flex inline-table">
+      <h1 className="text-3xl text-sky-50">
+        Moffu
+      </h1> 
+      <p className="text-base text-sky-200">
+        사이트 어떻게 만들지 생각하는 중..!
+      </p>
+    </div>
+  );
+}
