b2187e429bfd16cb3abbfe2405809f83cf7e1fdb
Uroyo <inertia_mailer_0c@icloud.com>
2025-03-29 19:55:56 +0900

    refactor: 메인 페이지 - 페이지 이동 버튼을 Nav 컴포넌트로 이동했습니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,21 @@
 import OvershootOut from '@/components/OvershootOut';
-import Button from '@/components/button';
+import Nav from '@/components/nav';
 
 export default function Home() {
   return (
-    <OvershootOut>
-      <div className="m-5 flex inline-table">
-        <h1 className="text-4xl text-sky-50">
-          Moffu ☁️
-        </h1> 
-        <p className="text-base text-sky-200">
-          아직이에요.
-        </p>
-        <div className="flex gap-2">
-          <Button variant='secondary' size='lg' href='/about'>소개</Button>
-          <Button variant='secondary' size='lg' href='/ui-test'>UI 테스트</Button>
-          <Button variant='secondary' size='lg' href='/fluffbox'>떨굼상자</Button>
+    <div>
+      <OvershootOut>
+        <div className="m-5 flex inline-table pt-12">
+          <h1 className="text-4xl text-sky-50">
+            Moffu ☁️
+          </h1> 
+          <p className="text-base text-sky-200">
+            아직이에요.
+          </p>
         </div>
-      </div>
-    </OvershootOut>
+      </OvershootOut>
+
+    </div>
+
   );
 }
