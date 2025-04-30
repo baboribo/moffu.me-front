fb28ba9abc6d21629c6e1a6a3efc8abe80b7ef49
urooyo <inertia_mailer_0c@icloud.com>
2025-04-30 22:05:58 +0900

    fix: 페이지에서 {post.body}가 표시되지 않는 문제를 해결함.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,7 @@
 export const dynamicParams = true;
 export const dynamic = 'force-dynamic'
 
-import { Any } from 'next-sanity';
+import { Any, PortableText } from 'next-sanity';
 import { getPosts } from '../../lib/sanity'
 
 export default async function BlogPage() {
@@ -10,14 +10,14 @@ export default async function BlogPage() {
   return (
     <div className="p-8 max-w-3xl mx-auto space-y-6 pt-20">
       <h1 className="text-white text-3xl font-bold">블로그</h1>
-      {posts.map((post:Any) => (
-        <div key={post.id} className="border-b pb-4">
+      {posts.map((post: Any, index: number) => (
+        <div key={`${post.id}-${index}`} className="border-b pb-4">
           <h2 className="text-white text-xl font-semibold">
             {post.title}
           </h2>
-          <p className="text-slate-200 text-base">
-            {post.body}
-          </p>
+          <a className="text-slate-200 text-base">
+            <PortableText value={post.body} />
+          </a>
           <p className="text-gray-500 text-sm">
             {new Date(post.publishedAt).toLocaleDateString()}
           </p>
