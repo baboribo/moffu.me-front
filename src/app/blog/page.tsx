5a39d24977e0713266024c51ffc19be95e962e4f
urooyo <inertia_mailer_0c@icloud.com>
2025-04-30 20:45:30 +0900

    refactor: sanity.ts 파일 이동경로 변경

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,7 @@ export const dynamicParams = true;
 export const dynamic = 'force-dynamic'
 
 import { Any } from 'next-sanity';
-import { getPosts } from '../lib/sanity'
+import { getPosts } from '../../lib/sanity'
 
 export default async function BlogPage() {
   const posts = await getPosts()
