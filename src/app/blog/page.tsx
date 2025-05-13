efeb63100ae27a5aed0f378b00e49d8a3c632210
urooyo <inertia_mailer_0c@icloud.com>
2025-05-14 07:08:19 +0900

    refactor?: import 분리

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,8 @@
 export const dynamicParams = true;
 export const dynamic = 'force-dynamic'
 
-import { Any, PortableText } from 'next-sanity';
+import { Any } from 'next-sanity';
+import { PortableText } from 'next-sanity';
 import { getPosts } from '../../lib/sanity'
 
 export default async function BlogPage() {
@@ -11,13 +12,13 @@ export default async function BlogPage() {
     <div className="p-8 max-w-3xl mx-auto space-y-6 pt-20">
       <h1 className="text-white text-3xl font-bold">나의 작은 기록장</h1>
       {posts.map((post: Any, index: number) => (
-        <div key={`${post.id}-${index}`} className="border-b pb-4">
+        <div key={`${post.id}-${index}` } className="border-b pb-4">
           <h2 className="text-white text-xl font-semibold">
             {post.title}
           </h2>
-          <a className="text-slate-200 text-base">
+          <div className="text-slate-200 text-base">
             <PortableText value={post.body} />
-          </a>
+          </div>
           <p className="text-gray-500 text-sm">
             {new Date(post.publishedAt).toLocaleDateString()}
           </p>
